8a1958ce52a7c43dbbe4132aa15514fadf195260
Vladislav Samsonov <vl4d.s4m@gmail.com>
2024-02-03 20:16:43 +0300

    Add program

---
+++ src/main/java/edu/tinkoff/IntegerAdder.java
@@ -0,0 +1,22 @@
+package edu.tinkoff;
+
+public class IntegerAdder {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("You must pass exactly two integer arguments.");
+            return;
+        }
+
+        int a;
+        int b;
+        try {
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Both arguments must be integers.");
+            return;
+        }
+
+        System.out.println(a + b);
+    }
+}
