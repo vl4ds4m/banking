a267360c9c4c2268d60952479138182011821f46
Vladislav Samsonov <vl4d.s4m@gmail.com>
2024-02-19 20:50:25 +0300

    Make kubernetes probes with spring-boot-actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
