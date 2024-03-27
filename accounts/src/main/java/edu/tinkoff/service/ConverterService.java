af9a13e85fa0752919ab4eb779028e3154797fe4
Vladislav Samsonov <vl4d.s4m@gmail.com>
2024-03-27 13:22:06 +0300

    Remove REST api for accounts and converter

--- accounts/src/main/java/edu/tinkoff/service/ConverterService.java
+++ accounts/src/main/java/edu/tinkoff/service/ConverterService.java
@@ -1,48 +1,22 @@
 package edu.tinkoff.service;
 
-import edu.tinkoff.dto.CurrencyMessage;
 import edu.tinkoff.dto.Currency;
 import edu.tinkoff.grpc.ConversionReply;
 import edu.tinkoff.grpc.ConversionRequest;
 import edu.tinkoff.grpc.ConverterServiceGrpc.ConverterServiceBlockingStub;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Service
 public class ConverterService {
-    private final RestTemplate restTemplate;
     private final ConverterServiceBlockingStub grpcStub;
 
-    private String converterUrl;
-
-    public ConverterService(
-            @Qualifier("auth") RestTemplate restTemplate,
-            ConverterServiceBlockingStub grpcStub
-    ) {
-        this.restTemplate = restTemplate;
+    public ConverterService(ConverterServiceBlockingStub grpcStub) {
         this.grpcStub = grpcStub;
     }
 
-    @Value("${services.converter.url}")
-    public void setConverterUrl(String converterUrl) {
-        this.converterUrl = converterUrl;
-    }
-
     public BigDecimal convert(Currency from, Currency to, BigDecimal amount) {
-        if (converterUrl.startsWith("http")) {
-            CurrencyMessage message = restTemplate.getForObject(
-                    converterUrl,
-                    CurrencyMessage.class,
-                    from, to, amount
-            );
-            return Objects.requireNonNull(message).amount();
-        }
-
         ConversionRequest request = ConversionRequest.newBuilder()
                 .setFrom(from.toString())
                 .setTo(to.toString())
