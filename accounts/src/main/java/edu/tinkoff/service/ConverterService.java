408da47cd99d5afd89e2daa6a707b784b4ae6402
Vladislav Samsonov <vl4d.s4m@gmail.com>
2024-03-28 13:48:49 +0300

    Add circuit breaker for converter service

--- accounts/src/main/java/edu/tinkoff/service/ConverterService.java
+++ accounts/src/main/java/edu/tinkoff/service/ConverterService.java
@@ -4,25 +4,34 @@ import edu.tinkoff.dto.Currency;
 import edu.tinkoff.grpc.ConversionReply;
 import edu.tinkoff.grpc.ConversionRequest;
 import edu.tinkoff.grpc.ConverterServiceGrpc.ConverterServiceBlockingStub;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class ConverterService {
-    private final ConverterServiceBlockingStub grpcStub;
+    private final Runnable conversion;
+    private ConversionRequest request;
+    private ConversionReply reply;
 
-    public ConverterService(ConverterServiceBlockingStub grpcStub) {
-        this.grpcStub = grpcStub;
+    public ConverterService(
+            ConverterServiceBlockingStub grpcStub,
+            CircuitBreaker circuitBreaker
+    ) {
+        conversion = circuitBreaker.decorateRunnable(
+                () -> reply = grpcStub.convert(request)
+        );
     }
 
     public BigDecimal convert(Currency from, Currency to, BigDecimal amount) {
-        ConversionRequest request = ConversionRequest.newBuilder()
+        request = ConversionRequest.newBuilder()
                 .setFrom(from.toString())
                 .setTo(to.toString())
                 .setAmount(amount.doubleValue())
                 .build();
-        ConversionReply reply = grpcStub.convert(request);
+
+        conversion.run();
         return BigDecimal.valueOf(reply.getAmount());
     }
 }
