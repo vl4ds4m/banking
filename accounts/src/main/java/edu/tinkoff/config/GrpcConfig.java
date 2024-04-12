0c0f6221662e06b0e9ba537c0cac5d016550b556
Vladislav Samsonov <vl4d.s4m@gmail.com>
2024-04-12 17:49:52 +0300

    Change class import

--- accounts/src/main/java/edu/tinkoff/config/GrpcConfig.java
+++ accounts/src/main/java/edu/tinkoff/config/GrpcConfig.java
@@ -1,13 +1,13 @@
 package edu.tinkoff.config;
 
-import edu.tinkoff.grpc.ConverterServiceGrpc.ConverterServiceBlockingStub;
+import edu.tinkoff.grpc.ConverterServiceGrpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.client.inject.GrpcClientBean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 @GrpcClientBean(
-        clazz = ConverterServiceBlockingStub.class,
+        clazz = ConverterServiceGrpc.ConverterServiceBlockingStub.class,
         beanName = "grpcStub",
         client = @GrpcClient("accounts")
 )
