fea97236248f116326c3a13531a8b985b55d6337
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-03-18 00:28:15 +0300

    Make single RestTemplateConfig

--- common/src/main/java/edu/vl4ds4m/banking/RestTemplateConfig.java
+++ common/src/main/java/edu/vl4ds4m/banking/RestTemplateConfig.java
@@ -1,8 +1,9 @@
-package edu.vl4ds4m.banking.rates;
+package edu.vl4ds4m.banking;
 
 import edu.vl4ds4m.banking.auth.Auth;
 import edu.vl4ds4m.banking.auth.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.convert.DurationUnit;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Configuration
 public class RestTemplateConfig {
-    private final long timeout;
+    private final Duration timeout;
 
-    public RestTemplateConfig(@Value("${services.timeout:0}") long timeout) {
+    public RestTemplateConfig(
+        @Value("${services.timeout:0}")
+        @DurationUnit(ChronoUnit.SECONDS)
+        Duration timeout
+    ) {
         this.timeout = timeout;
     }
 
@@ -24,8 +30,8 @@ public class RestTemplateConfig {
     @Primary
     public RestTemplate simpleRestTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
-            .connectTimeout(Duration.ofSeconds(timeout))
-            .readTimeout(Duration.ofSeconds(timeout))
+            .connectTimeout(timeout)
+            .readTimeout(timeout)
             .build();
     }
 
@@ -37,8 +43,8 @@ public class RestTemplateConfig {
         AuthInterceptor authInterceptor
     ) {
         return restTemplateBuilder
-            .connectTimeout(Duration.ofSeconds(timeout))
-            .readTimeout(Duration.ofSeconds(timeout))
+            .connectTimeout(timeout)
+            .readTimeout(timeout)
             .additionalInterceptors(authInterceptor)
             .build();
     }
