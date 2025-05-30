12a41a1886be8118cdbebdda0e985a1734f80fd5
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-05-30 08:55:07 +0300

    Add scripts fot test env

---
+++ test/halt.sh
@@ -0,0 +1,8 @@
+helm uninstall accounts
+helm uninstall converter
+helm uninstall rates
+helm uninstall keycloak
+kubectl delete secret realm-secret
+helm uninstall kafka
+helm uninstall redis
+helm uninstall postgresql
