d8b73a89458903eed3e7623ac9ab872632845c5c
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-07-05 14:58:55 +0300

    Update path to k8s configs in files for testing

--- test/k8s/deploy.sh
+++ test/k8s/deploy.sh
@@ -1,14 +1,14 @@
-helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f test/ci-cd/postgres-values.yml --wait
+helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql -f test/k8s/cfg/postgres-values.yml --wait
 
-helm install redis oci://registry-1.docker.io/bitnamicharts/redis -f test/ci-cd/redis-values.yml --wait
+helm install redis oci://registry-1.docker.io/bitnamicharts/redis -f test/k8s/cfg/redis-values.yml --wait
 
-helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka -f test/ci-cd/kafka-values.yml --wait
+helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka -f test/k8s/cfg/kafka-values.yml --wait
 
-kubectl create secret generic realm-secret --from-file=test/ci-cd/realm.json
-helm install keycloak keycloakx --repo https://codecentric.github.io/helm-charts --version 2.3.0 -f test/ci-cd/keycloak-values.yml --wait
+kubectl create secret generic realm-secret --from-file=test/k8s/cfg/realm.json
+helm install keycloak keycloakx --repo https://codecentric.github.io/helm-charts --version 2.3.0 -f test/k8s/cfg/keycloak-values.yml --wait
 
-helm install rates ./helm-chart -f test/ci-cd/rates-values.yml --wait
+helm install rates ./helm-chart -f test/k8s/cfg/rates-values.yml --wait
 
-helm install converter ./helm-chart -f test/ci-cd/converter-values.yml --wait
+helm install converter ./helm-chart -f test/k8s/cfg/converter-values.yml --wait
 
-helm install accounts ./helm-chart -f test/ci-cd/accounts-values.yml --wait
+helm install accounts ./helm-chart -f test/k8s/cfg/accounts-values.yml --wait
