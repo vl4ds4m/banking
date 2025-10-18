0210a1333ae17557446c9ab00130c06962649640
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-10-18 11:54:35 +0300

    Move k8s files

--- test/k8s/halt.sh
+++
@@ -1,8 +0,0 @@
-helm uninstall accounts
-helm uninstall converter
-helm uninstall rates
-helm uninstall keycloak
-kubectl delete secret realm-secret
-helm uninstall kafka
-helm uninstall redis
-helm uninstall postgresql
