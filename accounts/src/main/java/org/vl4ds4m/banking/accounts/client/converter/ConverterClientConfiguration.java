0edc8bcd5cec0d37aa0b6a3b71d4785acaff5ba4
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-10-25 21:05:59 +0300

    Update converter client bean

--- accounts/src/main/java/org/vl4ds4m/banking/accounts/client/converter/ConverterClientConfiguration.java
+++ accounts/src/main/java/org/vl4ds4m/banking/accounts/client/converter/ConverterClientConfiguration.java
@@ -3,14 +3,19 @@ package org.vl4ds4m.banking.accounts.client.converter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.vl4ds4m.banking.accounts.client.converter.invoker.ApiClient;
 
 @Configuration
 public class ConverterClientConfiguration {
 
     @Bean
-    public ConvertApi converterClient(@Value("${converter.url}") String url) {
-        var client = new ConvertApi();
-        client.getApiClient().setBasePath(url);
-        return client;
+    public ConvertApi converterClient(
+            @Value("${converter.url}") String url,
+            RestTemplate restTemplate
+    ) {
+        var client = new ApiClient(restTemplate);
+        client.setBasePath(url);
+        return new ConvertApi(client);
     }
 }
