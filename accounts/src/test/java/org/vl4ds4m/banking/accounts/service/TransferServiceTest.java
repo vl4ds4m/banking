ec10cfa6917a73b17eada5106a7def08f945acc3
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-11-05 23:04:29 +0300

    Add TransferServiceTest

---
+++ accounts/src/test/java/org/vl4ds4m/banking/accounts/service/TransferServiceTest.java
@@ -0,0 +1,114 @@
+package org.vl4ds4m.banking.accounts.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.vl4ds4m.banking.accounts.dao.AccountDao;
+import org.vl4ds4m.banking.accounts.entity.Account;
+import org.vl4ds4m.banking.accounts.service.expection.EntityNotFoundException;
+import org.vl4ds4m.banking.accounts.service.expection.ServiceException;
+import org.vl4ds4m.banking.common.entity.Currency;
+import org.vl4ds4m.banking.common.entity.Money;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+class TransferServiceTest {
+
+    private static final Account DEFAULT_SENDER = new Account(
+            111L,
+            Currency.USD,
+            Money.of(new BigDecimal("4376.12")));
+
+    private static final Account DEFAULT_RECEIVER = new Account(
+            222L,
+            Currency.EUR,
+            Money.of(new BigDecimal("27.05")));
+
+    @DisplayName("Перевод денег с одного счёта на другой")
+    @Test
+    void testTransferMoneyForEqualCurrencies() {
+        // Arrange
+        var money = Money.of(new BigDecimal("17.93"));
+        var convertedMoney = Money.of(new BigDecimal("15.84"));
+
+        var accountDao = mockAccountDao();
+
+        var converterService = mockConverterService();
+        when(converterService.convert(DEFAULT_SENDER.currency(), DEFAULT_RECEIVER.currency(), money))
+                .thenReturn(convertedMoney);
+
+        var service = new TransferService(accountDao, converterService);
+
+        // Act
+        var result = service.transferMoney(DEFAULT_SENDER.number(), DEFAULT_RECEIVER.number(), money);
+
+        // Assert
+        var senderMoney = DEFAULT_SENDER.money().subtract(money);
+        var receiverMoney = DEFAULT_RECEIVER.money().add(convertedMoney);
+
+        verify(accountDao).updateMoney(DEFAULT_SENDER.number(), senderMoney);
+        verify(accountDao).updateMoney(DEFAULT_RECEIVER.number(), receiverMoney);
+
+        assertEquals(senderMoney, result.totalSenderMoney());
+        assertEquals(receiverMoney, result.totalReceiverMoney());
+    }
+
+    @DisplayName("Ошибка при переводе денег с несуществующего счёта")
+    @Test
+    void testTransferMoneyFromAbsentAccountFailed() {
+        // Arrange
+        var service = new TransferService(mockAccountDao(), mockConverterService());
+
+        // Act & Assert
+        var e = assertThrows(EntityNotFoundException.class,
+                () -> service.transferMoney(999L, DEFAULT_RECEIVER.number(), Money.of(new BigDecimal("82.63"))));
+        assertTrue(e.getMessage().contains("999"));
+    }
+
+    @DisplayName("Ошибка при переводе денег на несуществующий счёт")
+    @Test
+    void testTransferMoneyToAbsentAccountFailed() {
+        // Arrange
+        var service = new TransferService(mockAccountDao(), mockConverterService());
+
+        // Act & Assert
+        var e = assertThrows(EntityNotFoundException.class,
+                () -> service.transferMoney(DEFAULT_SENDER.number(), 876L, Money.of(new BigDecimal("94.17"))));
+        assertTrue(e.getMessage().contains("876"));
+    }
+
+    @DisplayName("Ошибка при нехватке средств для перевода")
+    @Test
+    void testTransferMoneyOnShortageFailed() {
+        // Arrange
+        var service = new TransferService(mockAccountDao(), mockConverterService());
+
+        // Act & Assert
+        var e = assertThrows(ServiceException.class,
+                () -> service.transferMoney(
+                        DEFAULT_SENDER.number(),
+                        DEFAULT_RECEIVER.number(),
+                        Money.of(new BigDecimal("98000.25"))));
+        assertTrue(e.getMessage().contains("doesn't have enough money"));
+    }
+
+    private static AccountDao mockAccountDao() {
+        var dao = mock(AccountDao.class);
+
+        when(dao.existsByNumber(anyLong())).thenReturn(false);
+        when(dao.existsByNumber(DEFAULT_SENDER.number())).thenReturn(true);
+        when(dao.existsByNumber(DEFAULT_RECEIVER.number())).thenReturn(true);
+
+        when(dao.getByNumber(DEFAULT_SENDER.number())).thenReturn(DEFAULT_SENDER);
+        when(dao.getByNumber(DEFAULT_RECEIVER.number())).thenReturn(DEFAULT_RECEIVER);
+
+        return dao;
+    }
+
+    private static ConverterService mockConverterService() {
+        return mock(ConverterService.class);
+    }
+}
