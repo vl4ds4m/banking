148c6d67fb5be24aa986cbda19d176255df7a565
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-11-14 17:23:51 +0300

    Use spring http client timeout properties

--- common/src/main/java/org/vl4ds4m/banking/common/config/RestTemplateConfig.java
+++ common/src/main/java/org/vl4ds4m/banking/common/config/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package org.vl4ds4m.banking.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.convert.DurationUnit;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,41 +10,24 @@ import org.springframework.web.client.RestTemplate;
 import org.vl4ds4m.banking.common.auth.Auth;
 import org.vl4ds4m.banking.common.auth.AuthInterceptor;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
 @Configuration
 public class RestTemplateConfig {
-    private final Duration timeout;
-
-    public RestTemplateConfig(
-        @Value("${services.timeout:0}")
-        @DurationUnit(ChronoUnit.SECONDS)
-        Duration timeout
-    ) {
-        this.timeout = timeout;
-    }
 
     @Bean
     @Primary
     public RestTemplate simpleRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-            .connectTimeout(timeout)
-            .readTimeout(timeout)
-            .build();
+        return restTemplateBuilder.build();
     }
 
     @Bean
     @Auth
     @Profile(Auth.PROFILE)
     public RestTemplate restTemplateWithAuth(
-        RestTemplateBuilder restTemplateBuilder,
-        AuthInterceptor authInterceptor
+            RestTemplateBuilder restTemplateBuilder,
+            AuthInterceptor authInterceptor
     ) {
         return restTemplateBuilder
-            .connectTimeout(timeout)
-            .readTimeout(timeout)
-            .additionalInterceptors(authInterceptor)
-            .build();
+                .additionalInterceptors(authInterceptor)
+                .build();
     }
 }
