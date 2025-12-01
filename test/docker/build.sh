5bd667f0bbcb66ed9f69165ad5413e6c0e2470f0
Vladislav Samsonov <vl4d.s4m@gmail.com>
2025-12-01 18:46:13 +0300

    Update project build and description

---
+++ test/docker/build.sh
@@ -0,0 +1,4 @@
+PROJECT_DIR="../.."
+cp "$PROJECT_DIR/converter/target/converter.jar" ./build/converter.jar
+cp "$PROJECT_DIR/accounts/target/accounts.jar" ./build/accounts.jar
+cp "$PROJECT_DIR/web-ui/target/web-ui.jar" ./build/web-ui.jar
